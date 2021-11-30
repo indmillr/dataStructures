6c06131d55697e78042dedbcfee74dc8fee7b4b8
Ian Miller <theiandmiller@gmail.com>
2021-11-30 06:47:45 -0600

    Spread Operators

--- sandbox.js
+++ sandbox.js
@@ -35,10 +35,29 @@ console.log(
 
 // Copy Array Itmes using slice()
 function forecast(arr) {
-  let newArr = arr.slice(2, 3);
+  let newArr = arr.slice(2, 4);
   return newArr;
 }
 
 console.log(
   forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"]),
 );
+
+// Copy an Array with the Spread Operator
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+}
+console.log(copyMachine([true, false, true], 2));
+
+// Combine Arrays with the Spread Operator
+function spreadOut() {
+  let fragment = ["to", "code"];
+  let sentence = ["learning", ...fragment, "is", "fun"];
+  return sentence;
+}
+console.log(spreadOut());
