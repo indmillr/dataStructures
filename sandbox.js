36f4b2f63f9086d9ab260618058c6b20e4cf9a2e
Ian Miller <theiandmiller@gmail.com>
2021-11-30 20:52:31 -0600

    iterate through object keys and create an array of all object keys

--- sandbox.js
+++ sandbox.js
@@ -138,3 +138,104 @@ let userActivity = {
 };
 userActivity.data.online = 45;
 console.log(userActivity);
+
+// Access Property Names with Bracket Notation
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+}
+
+console.log(checkInventory("apples"));
+
+// Use the Delete Keyword to Remove Object Properties
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+
+console.log(foods);
+
+// Check if an Object has a specific property
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function isEveryoneHere(userObj) {
+  if (
+    userObj.hasOwnProperty("Alan") &&
+    userObj.hasOwnProperty("Jeff") &&
+    userObj.hasOwnProperty("Sarah") &&
+    userObj.hasOwnProperty("Ryan")
+  ) {
+    return true;
+  }
+  return false;
+}
+console.log(isEveryoneHere(users));
+
+// Iterate Through the Keys of an Object with a for...in Statement
+function countOnline(usersObj) {
+  let i = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      i++;
+    }
+  }
+  return i;
+}
+
+// Generate an Array of All Object Keys with Object.keys()
+let users = {
+  Alan: {
+    age: 27,
+    online: false,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: false,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function getArrayOfUsers(obj) {
+  return Object.keys(obj);
+}
+console.log(getArrayOfUsers(users));
