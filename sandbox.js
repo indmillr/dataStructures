3e5e7219cb31427045971a96ba76fa3bce76ed94
Ian Miller <theiandmiller@gmail.com>
2021-11-30 20:30:52 -0600

    add key-value pairs to an object

--- sandbox.js
+++ sandbox.js
@@ -115,3 +115,14 @@ let myNestedArray = [
 ];
 
 // Add Key-Value Pairs to Objects
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+
+console.log(foods);
